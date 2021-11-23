2c9c4fd5b5893d6a1691347ad067216cec68c22f
Marinella Moral <moral1820393@mkt.ceu.edu.ph>
2021-11-23 15:57:04 +0800

    Minimal Changes

---
+++ app/src/main/java/com/example/focusly/BreathingActivity.java
@@ -0,0 +1,18 @@
+package com.example.focusly;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BreathingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_breathing);
+
+    }
+
+    /*-- "CONTINUE TASK" will only be visible if the stress level goes down --*/
+    /*-- USE A GLOBAL VARIABLE FOR BIOFEEDBACK --*/
+}
